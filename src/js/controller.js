94955fd4c2988fc505d64dde6f909e449d5d95f4
kofdan <kofdan@gmail.com>
2023-01-28 14:59:57 +0200

    first commit

---
+++ src/js/controller.js
@@ -0,0 +1,148 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
+//
+
+// import icons from '../img/icons.svg';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { MODAL_CLOSE_SEC } from './config.js';
+
+// https://forkify-api.herokuapp.com/v2
+if (module.hot) {
+  module.hot.accept();
+}
+
+///////////////////////////////////////
+
+const conrolRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 0) Update results view to mark seelcted search result
+
+    resultsView.update(model.getSearchResultsPage());
+    // 1) Loading recipe
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
+
+    // 2) Rendering recipe
+    recipeView.render(model.state.recipe);
+    // 3)Updating bookmarks view
+
+    bookmarksView.update(model.state.bookmarks);
+  } catch (error) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResult(query);
+
+    // 3) Render results
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  // 1) Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render new pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const conrolServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  // 1) add/remove bookmark
+
+  if (!model.state.recipe.addBookmark) {
+    model.addBookmark(model.state.recipe);
+  } else {
+    model.deleteBookmark(model.state.id);
+  }
+  // 2) update recipe view
+  recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
+
+    // Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+
+    //  Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Success message
+    addRecipeView.renderMessage();
+
+    // Render bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+    // Change ID in URL
+    window.history.pushState(null, '', `${model.state.recipe.id}`);
+
+    // Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+
+    //
+  } catch (error) {
+    addRecipeView.renderError(error.message);
+  }
+};
+
+const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(conrolRecipes);
+  recipeView.addHandlerUpdateServings(conrolServings);
+  recipeView.addHandlerBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
+};
+init();
+
+const clearBookmarks = function () {
+  localStorage.clear('bookmarks');
+};
+
+clearBookmarks();
